427aba1794e11fa7daec073c4f08bf37c2c2f6f1
Sebas-crypt <usefullseba17@gmail.com>
2024-04-25 16:05:06 -0400

    fixing task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-complex types - list of floats
+Complex types - list of floats
 """
 from typing import List
 
 
-def sum_list(input_list: List[float]) -> floay:
+def sum_list(input_list: List[float]) -> float:
     """
     return a sum of all nums inside a list
     """
