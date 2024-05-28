5d2b93bdd828d2db8b02e159db9f0b80d2efd97b
Sebas-crypt <usefullseba17@gmail.com>
2024-05-28 13:42:13 -0400

    get list checker doesnt want to pass

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
-const getStudentIdsSum = (list) =>.reduce((total, student => total + student.id, 0);
-
-  export default getStudentIdsSum;
+export default function getStudentIdsSum(list) {
+  const num = list.map((id) => id.id);
+  return num.reduce((num1, num2) => num1 + num2, 0);
+}
